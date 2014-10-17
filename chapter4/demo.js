00906f3f9a6ece1fac92ae881913a125741bd3c9
EricDoug <liuxd6688@126.com>
2014-10-17 16:07:41 +0800

    first commit

---
+++ chapter4/demo.js
@@ -0,0 +1,30 @@
+/*
+undefined
+boolean
+
+string
+5.number
+int
+ 十进制    var box=100;
+ 八进制    var box=070;
+ 十六进制   var box=ox1f;
+float
+    var box=3.9;
+最小值     Number.MIN_VALUE
+最大值 Number.MAX_VALUE
+正无穷 Number.POSITIVE_INFINITY
+负无穷 Number.NEGATIVE_INFINITY
+isFinite(box)
+NaN(Not a Number)是一个特殊的值.
+
+6.object
+
+var box = new Object();
+
+function
+
+function box(){
+}
+*/
+
+document.write("<h1>Hello,world!</h1>");
